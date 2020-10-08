dcc51540010c421ec5539588c1533fb1a1cd8ff1
rohitpim1 <pimrohit33@gmail.com>
2020-10-08 12:46:02 +0530

    Updates

---
+++ precache-manifest.a3bcd0a893b5a7baaef5dda57aa2b70a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22366c358c1517dd4bd0cb5043bc26b3",
+    "url": "/DryFruits/index.html"
+  },
+  {
+    "revision": "c271a3c87cdd42e9b4b5",
+    "url": "/DryFruits/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "01f45d113fb15c7fbb08",
+    "url": "/DryFruits/static/css/main.704bb3df.chunk.css"
+  },
+  {
+    "revision": "c271a3c87cdd42e9b4b5",
+    "url": "/DryFruits/static/js/2.88d67f0e.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/DryFruits/static/js/2.88d67f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f45d113fb15c7fbb08",
+    "url": "/DryFruits/static/js/main.2e91e5c0.chunk.js"
+  },
+  {
+    "revision": "ccb76ae70a19368c7678",
+    "url": "/DryFruits/static/js/runtime-main.aa184d2a.js"
+  },
+  {
+    "revision": "9feb53a4048cf44806bd54da44dbed78",
+    "url": "/DryFruits/static/media/Anjeer.9feb53a4.jpg"
+  },
+  {
+    "revision": "62e613a9fe9392a5bffe1087582ff512",
+    "url": "/DryFruits/static/media/about.62e613a9.jpg"
+  },
+  {
+    "revision": "e3e3b9d8355aa5e0b51b4e41b7399731",
+    "url": "/DryFruits/static/media/background.e3e3b9d8.jpg"
+  },
+  {
+    "revision": "39b6924f79ba2a5d905a5e2eae08b503",
+    "url": "/DryFruits/static/media/best.39b6924f.png"
+  },
+  {
+    "revision": "8cd5cfdd0c0c5cb2c3b072ac00c3f6b1",
+    "url": "/DryFruits/static/media/kismis.8cd5cfdd.jpg"
+  },
+  {
+    "revision": "662f5757265303d8bef1a73abcd30de6",
+    "url": "/DryFruits/static/media/premium.662f5757.jpg"
+  }
+]);
